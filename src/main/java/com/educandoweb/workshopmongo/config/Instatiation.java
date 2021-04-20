73f2fb1f9a73eb44d4e6e2b5d618fc2f59a80cc8
marcio.frainer <marcio.frainer@softplan.com.br>
2021-04-20 10:43:27 -0300

    Criacao de Post

--- src/main/java/com/educandoweb/workshopmongo/config/Instatiation.java
+++ src/main/java/com/educandoweb/workshopmongo/config/Instatiation.java
@@ -1,12 +1,17 @@
 package com.educandoweb.workshopmongo.config;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.educandoweb.workshopmongo.domain.Post;
 import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.repository.PostRepository;
 import com.educandoweb.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,16 +19,42 @@ public class Instatiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private PostRepository postRepository;
+
+	private User maria;
+	private User alex;
+	private User bob;
+	
+	
+	private void DefineUsers() {
+		userRepository.deleteAll();
+		
+		maria = new User(null, "Maria Brown", "maria@gmail.com");
+		alex = new User(null, "Alex Green", "alex@gmail.com");
+		bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));		
+	}
+	
+	private void DefinePosts() throws ParseException {
+		postRepository.deleteAll();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+	}
 	
 	@Override
 	public void run(String... args) throws Exception {
-		userRepository.deleteAll();
-		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		DefineUsers();
+		DefinePosts();
 	}
 
 }
