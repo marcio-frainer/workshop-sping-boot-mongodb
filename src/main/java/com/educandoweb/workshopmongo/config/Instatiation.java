cec73acbc418c3264061d846ddaf965195409486
marcio.frainer <marcio.frainer@softplan.com.br>
2021-04-22 09:50:29 -0300

    Adicionando comentario aos posts

--- src/main/java/com/educandoweb/workshopmongo/config/Instatiation.java
+++ src/main/java/com/educandoweb/workshopmongo/config/Instatiation.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import com.educandoweb.workshopmongo.domain.Post;
 import com.educandoweb.workshopmongo.domain.User;
 import com.educandoweb.workshopmongo.dto.AuthorDTO;
+import com.educandoweb.workshopmongo.dto.CommentDTO;
 import com.educandoweb.workshopmongo.repository.PostRepository;
 import com.educandoweb.workshopmongo.repository.UserRepository;
 
@@ -29,7 +30,9 @@ public class Instatiation implements CommandLineRunner {
 	private User bob;
 	
 	private Post post1; 
-	private Post post2; 	
+	private Post post2; 
+	
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	private void DefineUsers() {
 		userRepository.deleteAll();
@@ -42,9 +45,8 @@ public class Instatiation implements CommandLineRunner {
 	}
 	
 	private void DefinePosts() throws ParseException {
-		postRepository.deleteAll();
+		postRepository.deleteAll();		
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
@@ -56,10 +58,22 @@ public class Instatiation implements CommandLineRunner {
 		userRepository.save(maria);
 	}
 	
+	private void DefineComments() throws ParseException {
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+	
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
+	}
+	
 	@Override
 	public void run(String... args) throws Exception {
 		DefineUsers();
 		DefinePosts();
+		DefineComments();
 	}
 
 }
--- src/main/java/com/educandoweb/workshopmongo/domain/Post.java
+++ src/main/java/com/educandoweb/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.educandoweb.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.educandoweb.workshopmongo.dto.AuthorDTO;
+import com.educandoweb.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,7 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -97,6 +101,12 @@ public class Post implements Serializable{
 		return true;
 	}
 	
-	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}	
 
 }
